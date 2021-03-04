d11166165cc4a5e52625c6daa0bde7c7b0ea62be
n0v11 <36411454+n0v11@users.noreply.github.com>
2021-03-05 02:15:37 +0300

    tt

--- CoreAppWithEf/Program.cs
+++ CoreAppWithEf/Program.cs
@@ -14,7 +14,6 @@ namespace CoreAppWithEf
                 Console.WriteLine($"Id - {e.Id}, Name - {e.Name}, Age - {e.Age}");
             }
             Console.ReadKey();
-            //Actions.Add("Petr", 46); // пример добавления новой записи в бд. Есть вариант программы где реализованы дополнительно update и delete функции.
             Console.Clear();
             DbActions.Select();
         }
